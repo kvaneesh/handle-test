3ff75f4890faaa57ae99e9c9ab6f1e6fddba4611
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2010-11-12 19:34:27 +0530

    Add O_PATH test
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TARGETS=append_handle create_handle_from_fd create_handle hardlink_handle \
 	read_handle truncate_handle hardlink_unlink_file_handle \
-	chown_handle
+	chown_handle o_path_test
 all: ${TARGETS}
 
 clean:
---
+++ o_path_test.c
@@ -0,0 +1,37 @@
+/*
+ *   Copyright (C) International Business Machines  Corp., 2010
+ *   Author(s): Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation; either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "handle.h"
+
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	char buf[10];
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "Usage: %s, <filename>\n", argv[0]);
+		exit(1);
+	}
+	fd = open(argv[1], O_PATH);
+	if (read(fd, buf, 10) >= 0)
+		printf("Test failed\n");
+	return 0;
+}
